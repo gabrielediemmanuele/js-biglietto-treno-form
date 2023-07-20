962d30e597d07b575cca38d0727dcb07c3fea9fb
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-20 15:00:23 +0200

    feat: add structure to js

---
+++ main.js
@@ -0,0 +1,14 @@
+//Steps
+// Step 1 - Creare una const per il value dell'input "km".
+const distanceKM = document.getElementById("distanceKM");
+// Step 2 - Creare una const per il value dell'input "age".
+const userAge = document.getElementById("userAge");
+// Step 3 - Creare una const per il bottone.
+const sendInfoButton = document.getElementById("send-info");
+// Step 4 - Creare una const per il testo del biglietto
+const ticketText = document.getElementById("ticket-text");
+
+// Prezzo del biglietto
+let finalPrice = distanceKM * 0.21;
+
+sendInfoButton.addEventListener("click", function () {});
