ed796a59688cf652c27fc174ec4eb93e739e80ff
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-20 15:48:57 +0200

    feat: app function  - complete

--- main.js
+++ main.js
@@ -9,6 +9,20 @@ const sendInfoButton = document.getElementById("send-info");
 const ticketText = document.getElementById("ticket-text");
 
 // Prezzo del biglietto
-let finalPrice = distanceKM * 0.21;
+/* let finalPrice = distanceKM.value * 0.21; */
 
-sendInfoButton.addEventListener("click", function () {});
+sendInfoButton.addEventListener("click", function () {
+  let finalPrice = distanceKM.value * 0.21;
+
+  const userAgeValue = parseInt(userAge.value);
+
+  if (userAgeValue < 18) {
+    finalPrice = finalPrice - (finalPrice * 20) / 100;
+  } else if (userAgeValue >= 65) {
+    finalPrice = finalPrice - (finalPrice * 40) / 100;
+  }
+
+  // aggiungerli al paragrafo ticket-text
+  ticketText.innerHTML =
+    "Il prezzo del tuo biglietto è " + finalPrice.toFixed(2) + " €";
+});
